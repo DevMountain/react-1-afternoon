136ac653dbdbd1c3b593124e884d743a3a710c3a
James Lemire <webdev.jameslemire@gmail.com>
2017-04-21 10:36:39 -0600

    JL master - material-ui.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
-    <MuiThemeProvider>
-
-    </MuiThemeProvider>
+    
   }
 }
 
